988deb6468d26de67186815a2026649e4ed8bfa8
Dante Marinho <dantecnia@hotmail.com>
2022-05-30 02:35:21 +0100

    Final primeiro-projeto-node.

--- Node/Chapter I/Fundamentos do NodeJS/primeiro-projeto-node/README.md
+++ Node/Chapter I/Fundamentos do NodeJS/primeiro-projeto-node/README.md
@@ -6,28 +6,34 @@
 
 - [x] Deve ser possível criar umma conta
 - [x] Deve ser possível buscar o extrato bancário (statement) do cliente
-- [] Deve ser possível realizar um depósito
-- [] Deve ser possível realizar um saque
-- [] Deve ser possível buscar o extrato bancário do cliente por data
-- [] Deve ser possível atualizar dados da conta do cliente
-- [] Deve ser possível obter dados da conta do cliente
-- [] Deve ser possível deletar uma conta
+- [x] Deve ser possível realizar um depósito
+- [x] Deve ser possível realizar um saque
+- [x] Deve ser possível buscar o extrato bancário do cliente por data
+- [x] Deve ser possível atualizar dados da conta do cliente
+- [x] Deve ser possível obter dados da conta do cliente
+- [x] Deve ser possível deletar uma conta
+- [x] Deve ser possível retornar o balance
 
 ---
 
 ## Regras de negócio
 
 - [x] Não deve ser possível cadastrar uma conta com CPF já existente
+- [x] Não deve ser possível buscar extrato em uma conta não existente
 - [x] Não deve ser possível fazer depósito em uma conta não existente
-- [] Não deve ser possível buscar extrato em uma conta não existente
-- [] Não deve ser possível fazer saque em uma conta não existente
-- [] Não deve ser possível excluir uma conta não existente
+- [x] Não deve ser possível fazer saque em uma conta não existente
+- [x] Não deve ser possível excluir uma conta não existente
 - [] Não deve ser possível fazer saque quando o saldo for insuficiente
 
 ## Pacotes instalados
 
 - yarn add uuid
 
+## Como rodar a app
+
+Duas maneiras:
+- yarn dev
+- nodemon src/index.js
 
 ## Middlewares
 
@@ -35,7 +41,6 @@ Como definimos que uma função vai ser um Middleware?
 » Precisa receber 3 params: `request, response, next`. O `next` é que irá definir se o Middleware prossegue com a operação ou se vai parar.
 
 ```js
-
 // Criação de um Middleware
 function verifyIfExistsAccountCPF(request, response, next) {
     const { cpf } = request.params
--- Node/Chapter I/Fundamentos do NodeJS/primeiro-projeto-node/src/index.js
+++ Node/Chapter I/Fundamentos do NodeJS/primeiro-projeto-node/src/index.js
@@ -14,7 +14,7 @@ const customers = [];
 // ------------------------------------------
 
 function verifyIfExistsAccountCPF(request, response, next) {
-    const { cpf } = request.params
+    const { cpf } = request.headers
 
     const customer = customers.find(customer => customer.cpf === cpf);
 
@@ -26,6 +26,20 @@ function verifyIfExistsAccountCPF(request, response, next) {
     request.customer = customer // quem chamar o Middleware verifyIfExistsAccountCPF vai ter acesso ao request.customer
 
     return next(); // Manda prosseguir, pois existe um customer (o Middleware não encontrou nenhum problemas nas verificações)
+}
+
+function getBalance(statement) {
+    // Usar o reduce() para calcular o que recebeu e o que gastou
+
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
 }
 
 // ------------------------------------------
@@ -62,11 +76,97 @@ app.post('/account', (request, response) => {
 
 // app.use(verifyIfExistsAccountCPF); // neste caso, todas as rotas que se encontra abaixo irão utilizar esse Middleware
 
-// Obter o estrato bancário a partir de um CPF :: statement é o estrato bancário
-app.get('/statement/:cpf', verifyIfExistsAccountCPF, (request, response) => {
+// Obter o extrato bancário (geral) :: statement é o extrato bancário
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request;
-    
+    // console.log(customer)
+
     return response.json(customer.statement)
+})
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request; // recupera o user, já validando se a conta é válida
+
+    // Vamos chamar o depósito e o saque de "operações"
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit" // credit | debit
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
+// Realizar saque
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" })
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit" // credit | debit
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
+// Obter o extrato bancário a partir de uma data :: statement é o extrato bancário
+app.get('/statement/date', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00"); // buscar pela data independente da hora da transação ("00:00" é um hack para obter qualquer horário do dia)
+    // console.log(customer)
+
+    const statement = customer.statement.filter(statement => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+    return response.json(statement)
+})
+
+app.put('/account', verifyIfExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send(customer);
+})
+
+app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.send(customer);
+})
+
+app.delete('/account', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    // splice
+    customers.splice(customer, 1);
+
+    return response.send(customers);
+})
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance)
 })
 
 app.listen(3333)
